34f6b642b38eaaad5b07e8459924589644da9ec5
Dev Kalra <kalradev@outlook.com>
2020-08-01 15:25:13 +0530

    l1 c1

---
+++ l1-basics/chal1.py
@@ -0,0 +1,20 @@
+# Convert hex to base64
+
+#### CRYPTOPALS RULE ####
+# Always operate on raw bytes, never on encoded strings. 
+# Only use hex and base64 for pretty-printing. 
+
+import base64
+
+def hexTobase64(hexString):
+    # get bytes from hex
+    byteString = bytes.fromhex(hexString)
+    # base64 encoding
+    encodedString = base64.b64encode(byteString)
+    # encoded byte string 
+    return encodedString
+
+# input hex string
+hexString = input()
+
+print(hexTobase64(hexString).decode())
